e0779993304b45c3abe4b70f5d75ab9413c85c23
Matthew Schuiteman <matthewschuiteman@gmail.com>
2021-11-10 22:01:34 -0800

    Initial Commit

---
+++ FishHouse/FishHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishHouseApp.swift
+//  FishHouse
+//
+//  Created by Matthew Schuiteman on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FishHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
