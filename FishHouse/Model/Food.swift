73b4bf6ca9838709712e052b2f49548055f12d2a
Matthew Schuiteman <matthewschuiteman@gmail.com>
2022-05-19 14:48:16 -0700

    Added commented changes and expanded requests to fish data structure

--- FishHouse/Model/Food.swift
+++ FishHouse/Model/Food.swift
@@ -11,9 +11,27 @@ struct Food: Identifiable
 {
     //Unique name to identify the food
     let id: String
+    // Abbreviation for user convienence, instead of saying "Pacfic Herring" say "PAC"
     let abbrev: String
+    // Weight of the box that the fish comes in, needed for breakout calculation
     var box_weight: Int
+    // Are we adding on an extra box of fish?
     var add_on: Bool
+    // -- Requests --
+    // Amount that show requests
     var show_amount: Int
-    var point_amount: Int
+    //Amount that dolphin point requests
+    var point: Int
+    //Etc...
+    var pinn: Int
+    
+    var otter: Int
+    
+    var whale: Int
+    
+    var aquariums: Int
+    
+    var rescue: Int
+    
+    var wild: Int
 }
