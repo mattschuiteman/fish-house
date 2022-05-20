e73304f3a9547ae5d721957c60b250ce9731a178
Matthew Schuiteman <matthewschuiteman@gmail.com>
2022-05-19 18:18:20 -0700

    Added new type ParkLocation to make back end of apps query easier and better formatted and stuff

---
+++ FishHouse/Model/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  FishHouse
+//
+//  Created by Matthew Schuiteman on 5/19/22.
+//
+
+import Foundation
+
+enum ParkLocation {
+    case show
+    case point
+    case pinn
+    case otter
+    case whale
+    case aquariums
+    case rescue
+    case wild
+}
